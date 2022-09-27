18af6ac2239c3da936fd7abc627dd5c250f435b8
Jorge Alvarez <jalvarez.y.89@hotmail.com>
2022-09-27 15:25:04 -0500

    cambios

--- sima.App/sima.App.Persistencia/Repositorio/ContextDb.cs
+++ sima.App/sima.App.Persistencia/Repositorio/ContextDb.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using sima.App.Dominio; 
+using sima.App.Dominio;
 
 namespace sima.App.Persistencia
 {
+
     public class ContextDb : DbContext
     {
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -15,22 +16,21 @@ namespace sima.App.Persistencia
             .HasKey(b => b.EmployeeId)
             .HasName("PrimaryKey_EmployeeId");
         }
-            public DbSet<Employee> Employees { get; set; } =null!;
-            public DbSet<User> Users { get; set; }= null!;
-            public DbSet<Customer> Customers { get; set; } =null!;
-            public DbSet<Car> Cars {get; set; }= null!;
-            //public DbSet<ServiceRequest> ServiceRequests {get; set; }= null!;
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Car> Cars { get; set; } = null!;
+        //public DbSet<ServiceRequest> ServiceRequests {get; set; }= null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
             {
-                if (!optionsBuilder.IsConfigured)
-                {
-                    optionsBuilder.UseSqlServer($"Server=simadb.ck9lvdmqxsey.us-east-1.rds.amazonaws.com; Database=simadb; User id=admin; Password=@ircrafT89!");
-                }
-            
-            //base.Onconfiguring(optionsBuilder);
+                optionsBuilder.UseSqlServer("Server=tcp:server-sima.database.windows.net,1433;Initial Catalog=db-sima;Persist Security Info=False;User ID=adminsima;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
-        
+            //base.Onconfiguring(optionsBuilder);
+        }
+
     }
 }
 
-    
+
